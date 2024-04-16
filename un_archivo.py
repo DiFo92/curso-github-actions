d00db928a5fe31a8b9bb235346f7734be5e8cbd7
Diego Forero <diego.forero@scrd.gov.co>
2024-04-16 16:50:41 -0500

    corrección en un_archivo.py

--- un_archivo.py
+++ un_archivo.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 def main():
-    nombre = os.getenv("USERNAME") | "DiFo"
+    nombre = os.getenv("USERNAME")  # | "DiFo"
     print(f"¡Hola, {nombre} desde GitHub!")
     print("Hola mundo desde python actualizado en: ")
     print(datetime.now())
